db8048b2fa968e9324959948a4e01b0bdddf531f
schwartziel <jadenmschwartz00@gmail.com>
2024-02-05 14:11:51 -0600

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("jschwartz") -- this is where neovim needs to look to find the luafiles, ~/.config/nvim/lua/jschwartz/(luafiles)
+require("jschwartz.remap") -- remap for keybindings, like the leaderkey.. which as of now is ","
+require("jschwartz.set") -- this is for basic settings like relative number
+-- print("hello world") 
+-- I'm keeping this here idk why but I think it's kind of funny that if I were to 
+-- uncomment this line everytime you open vim it would say hello world 
+
